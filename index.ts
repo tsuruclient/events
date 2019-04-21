cb8866f250d58308468134f868f3e7f4008ea4d9
Origami ONOUE <onoueorigami@gmail.com>
2019-04-21 15:31:05 +0900

    はい

--- index.ts
+++
@@ -1,4 +0,0 @@
-import * as rEvents from "./src/renderer";
-import * as iEvents from "./src/ipc";
-export const rendererEvents = rEvents;
-export const ipcEvents = iEvents;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import * as rEvents from "./renderer";
+import * as iEvents from "./ipc";
+export const rendererEvents = rEvents;
+export const ipcEvents = iEvents;
